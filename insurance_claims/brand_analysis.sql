77a3cd6682369963a159371ff674b08f7b899d59
darnik-data <dar.nik.data@gmail.com>
2026-02-06 21:14:49 +0500

    Create brand_analysis.sql

---
+++ insurance_claims/brand_analysis.sql
@@ -0,0 +1,8 @@
+/* Вывести список всех марок машин, у которых  общая сумма одобренных выплат превышает 100 000 */
+
+SELECT car_brand, 
+SUM (claim_amount) AS total_car
+FROM insurance_claims
+WHERE status = 'Approved
+GROUP BY car_brand
+HAVING UM (claim_amount) > 100000;
