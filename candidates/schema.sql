2e4fbc0cad748e69d094c4d9158717eb29d03cc4
darnik-data <dar.nik.data@gmail.com>
2026-02-08 21:57:48 +0500

    Create schema.sql

---
+++ candidates/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE candidates (
+ id INT PRIMARY KEY AUTO_INCREMENT,
+ full_name VARCHAR (255) NOT NULL,
+ age INT NOT NULL,
+ education VARCHAR (255) NOT NULL,
+ experience_years INT NOT NULL,
+ current_skills VARCHAR (255) NOT NULL
+);
